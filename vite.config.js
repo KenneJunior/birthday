28372804de7d9c1ee82a9d1a7a1d1aa26482cf8a
Kenne junior <fouomenekennebonifacejunior@gmail.com>
2025-09-19 04:08:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { qrcode } from 'vite-plugin-qrcode'
 export default defineConfig({
     server: {
         host: '0.0.0.0',
-        allowedHosts: ['cf51d173fb50.ngrok-free.app','5c210a6d7b1f.ngrok-free.app', 'localhost'],
         port: 5173
 
     },
